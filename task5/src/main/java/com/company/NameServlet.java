2c98e3356ab6821df0419f19ea34f089fe1becae
Uliana <u.gorochovich@gmail.com>
2020-05-26 12:16:28 +0300

    add task5

---
+++ task5/src/main/java/com/company/NameServlet.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class NameServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String name = request.getParameter("name");
+        response.getOutputStream().println("Name is " + name + "!");
+    }
+}
